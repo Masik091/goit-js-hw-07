f4f0e6be1647e5024d60428af45c4f07be09d56a
Masik091 <maliastep155@gmail.com>
2024-06-30 18:52:42 +0300

    update

--- js/task-6.js
+++ js/task-6.js
@@ -7,30 +7,38 @@ function getRandomHexColor() {
 }
 document.addEventListener('DOMContentLoaded', function () {
   const input = document.querySelector('#controls input');
-  const button = document.querySelector('[data-create]');
+  const createButton = document.querySelector('[data-create]');
   const destroyButton = document.querySelector('[data-destroy]');
   const boxes = document.querySelector('#boxes');
-
+  
   function createBoxes(amount) {
     boxes.innerHTML = '';
-
     let size = 30;
+    const containerNewBoxes = document.createDocumentFragment();
     for (let i = 0; i < amount; i++) {
       const box = document.createElement('div');
       box.style.width = `${size}px`;
       box.style.height = `${size}px`;
       box.style.backgroundColor = getRandomHexColor();
-      boxes.appendChild(box);
+      containerNewBoxes.appendChild(box);
       size += 10;
     }
-  
+    boxes.appendChild(containerNewBoxes);
   }
-
-  button.addEventListener('click', function () {
+  
+  function destroyBoxes() {
+    boxes.innerHTML = '';
+  }
+  
+  createButton.addEventListener('click', function () {
     const amount = parseInt(input.value);
     if (amount >= 1 && amount <= 100) {
       createBoxes(amount);
       input.value = '';
     }
+  });
+  
+  destroyButton.addEventListener('click', function () {
+    destroyBoxes();
   });
 });
